591ed0525e5160d8a414ca82591566e34a1a9c44
Nugraha Panca Wibisana <nugrahapancawibisana@gmail.com>
2026-01-05 09:30:52 +0700

    fix: fix logic release_date

--- 6. FILTER_MOVIES_BY_NAME_&_GENRES_WITH_PAGINATION.sql
+++ 6. FILTER_MOVIES_BY_NAME_&_GENRES_WITH_PAGINATION.sql
@@ -16,7 +16,7 @@ FROM
     JOIN movie_genres mg ON m.id = mg.movie_id
     JOIN genres g ON mg.genres_id = g.id
 WHERE
-    release_date >= CURRENT_DATE
+    release_date <= CURRENT_DATE
     AND m.title LIKE 'title%'
     AND g.name IN ('Action', 'Adventure', 'Sci-Fi')
 GROUP BY
